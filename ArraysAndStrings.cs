ded292894fd0f81cf880b6291f497817f4ba0899
Remi <remi3195@gmail.com>
2021-01-22 16:01:16 +0100

    Add project files.

---
+++ ArraysAndStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cracking.ArraysAndStrings
+{
+    //  Is Unique: Implement an algorithm to determine if a string has all unique characters.What if you
+    //  cannot use additional data structures?
+
+   [Entry]
+    public class Exercise1 : IExercise
+    {
+        public static bool IsUnique(string s)
+        {
+
+        }
+
+        public void Main()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Cracking
+{
+    public class Program
+    {
+        public static List<IExercise> exercies;
+        public static IExercise current;
+
+        private static void Main()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var types = assembly.GetTypes();
+
+            foreach (var type in types)
+            {
+                if (type.GetCustomAttribute<EntryAttribute>() == null) continue;
+                if (type.IsGenericType) continue;
+                if (type.IsAbstract) continue;
+
+                current = Activator.CreateInstance(type, true) as IExercise;
+
+                break;
+            }
+
+            current.Main();
+        }
+    }
+
+    public interface IExercise
+    {
+        void Main();
+    }
+
+    public class EntryAttribute : Attribute { }
+}
